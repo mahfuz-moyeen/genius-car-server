8f211f9cc58e920cc148f3676dfd56c661d907bd
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-04-29 01:46:49 +0600

    aa

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const { send } = require('express/lib/response');
-const ObjectId = require('mongodb').ObjectId;
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 
-const port = process.env.PROT || 5000;
 const app = express();
 
 // middleware
@@ -34,13 +32,6 @@ function verifyJWT(req, res, next) {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.irqio.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-console.log('mongo db runinng');
-//   // perform actions on the collection object
-//   client.close();
-// });
 
 async function run() {
     try {
