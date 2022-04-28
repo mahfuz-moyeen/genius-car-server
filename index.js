a7ed1c5cc7bdde8559a628e444218c164e2f685c
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-04-29 02:00:56 +0600

    fix heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+//user this or get error in heroku
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
